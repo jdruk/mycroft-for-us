79a6206daf0c481623dc20bff44fbbf790c2730c
jdruk <devjosafa@gmail.com>
2019-03-18 14:26:56 -0300

    add redirecionamento pos-login

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+	protected
+    def after_sign_in_path_for(resource)
+    	dashboard_path || request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+    end
 end
