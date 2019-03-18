e5bd37bd016bd75a3b1882a01c70ebd8a04bf289
jdruk <devjosafa@gmail.com>
2019-03-18 11:26:35 -0300

    modificado routes e add icones dos menus: concentradores, planos de dados e etc

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  resources :links
+  
   resources :concentrators
   resources :plan_of_data
-  resources :addresses
-  resources :clients
-  devise_for :users, :controllers => {registrations: 'users/registrations', sessions: 'users/sessions'}
-  root "index#index"
-  get 'dashboard', to: "index#dashboard"
 
+  resources :clients do
+    resources :addresses do 
+      resources :links
+    end
+  end
+
+  devise_for :users, :controllers => {registrations: 'users/registrations', sessions: 'users/sessions'}
+  
+  root "index#index"
+  
+  # Dashboard
+  get 'dashboard', to: "index#dashboard"
   scope "/administrator" do 
   	resources :users
     post 'user_change/:id', to: "users#change_approved", as: 'change_user'
