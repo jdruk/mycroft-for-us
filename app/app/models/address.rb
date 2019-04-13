04e7416d68e6ddbe7da53be107a77e5835737f46
jdruk <devjosafa@gmail.com>
2019-04-13 13:52:16 -0300

    fixed mapa em endereço

--- app/app/models/address.rb
+++ app/app/models/address.rb
@@ -1,22 +1,31 @@
 class Address < ApplicationRecord
   belongs_to :client
 
-  geocoded_by :search_for
+  geocoded_by :search_address
   after_validation :geocode, if: :updated?
 
   def updated?
-  	address_changed? || neighborhood_changed || city_changed? || state_changed?
+  	address_changed? || neighborhood_changed? || city_changed? || state_changed?
   end
 
-  def search_for
-  	[address, number,neighborhood,city, state].
-  		reject(&:nil?).
-  		reject(&:empty?).
-  		join(',')
+  def search_address
+  	values_to_address.join(',')
+  end
+
+
+  def parser_address_to_url
+    ERB::Util.url_encode(values_to_address.join(' '))
   end
 
   def to_s
-  	search_for 
+    search_address 
   end
+
+  private
+    def values_to_address
+      [address, number,neighborhood,city, state].
+        reject(&:nil?).
+        reject(&:empty?)
+    end
 
 end
--- app/app/models/concentrator.rb
+++ app/app/models/concentrator.rb
@@ -1,2 +1,12 @@
 class Concentrator < ApplicationRecord
+
+	# validações 
+	validates :hostname, presence: true
+	validates :address, presence: true
+	validates :login, presence: true
+	validates :password, presence: true
+
+	def online?
+		false
+	end
 end
