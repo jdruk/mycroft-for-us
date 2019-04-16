2372afac86dff45b9d41e565f0623c276b9917d0
jdruk <devjosafa@gmail.com>
2019-04-16 13:31:13 -0300

    fixed bug user#index

--- app/app/controllers/users_controller.rb
+++ app/app/controllers/users_controller.rb
@@ -43,7 +43,6 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.js 
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
